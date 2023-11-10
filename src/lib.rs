cca12a363e7410200514cc9804dd4ced2e97f015
Kristoffer Ödmark <kristoffer.odmark90@gmail.com>
2023-11-10 09:25:04 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 pub mod prelude {
-    pub use crate::traits::{GetBTreeMap, GetHashMap, GetOwned, KeyValueStore, MutKeyValueStore};
+    pub use crate::traits::{GetBTreeMap, GetHashMap, GetOwned, KeyValueStore};
 }
 
 pub mod traits;
 
+#[cfg(feature = "impl")]
 pub mod std_impl;
 
+#[cfg(feature = "impl")]
 pub mod parking_lot;
 
 //mod itertest;
@@ -31,40 +33,6 @@ pub mod test {
                 None => assert!(true, "should be a value here"),
             })
             .ok();
-        assert_eq!(s, "hello".to_string());
-
-        kvstore
-            .mutate(&1, |v| match v {
-                Some(v) => {
-                    // can mutate the value
-                    *v = "world".to_string()
-                }
-                None => {}
-            })
-            .ok();
-
-        kvstore
-            .inspect(&1, |v| assert_eq!(Some("world"), v.map(|x| x.as_str())))
-            .ok();
-    }
-
-    pub fn test_impl_mut<T: MutKeyValueStore<usize, String>>(mut kvstore: T) {
-        kvstore.insert(1, String::from("hello")).ok();
-        kvstore
-            .inspect(&1, |v| assert_eq!(Some("hello"), v.map(|x| x.as_str())))
-            .ok();
-
-        kvstore.inspect(&2, |v| assert_eq!(None, v)).ok();
-
-        // can use clone the value
-        let mut s = String::new();
-        kvstore
-            .inspect(&1, |v| match v {
-                Some(v) => s = v.clone(),
-                None => assert!(true, "should be a value here"),
-            })
-            .ok();
-
         assert_eq!(s, "hello".to_string());
 
         kvstore
--- src/traits.rs
+++ src/traits.rs
@@ -1,7 +1,13 @@
+use async_trait::async_trait;
 use std::collections::{BTreeMap, HashMap};
 
 /// trait to give the most basic of key value store functionality to immutable objects
-pub trait KeyValueStore<K, V> {
+#[async_trait]
+pub trait KeyValueStore<K, V>
+where
+    K: Send + Sync,
+    V: Send,
+{
     type Err;
     /// Inserts a key-value pair into the map.
 
@@ -10,168 +16,125 @@ pub trait KeyValueStore<K, V> {
     /// If the map did have this key present, the value is updated, and the old value is returned.
     /// The key is not updated, though; this matters for types that can be == without
     /// being identical.
-    fn insert(&self, key: K, value: V) -> Result<Option<V>, Self::Err>;
+    async fn insert(&self, key: K, value: V) -> Result<Option<V>, Self::Err>;
 
     /// Removes an entry from the map, returning the key and value if they existed in the map.
-    fn remove(&self, key: &K) -> Result<Option<V>, Self::Err>;
+    async fn remove(&self, key: &K) -> Result<Option<V>, Self::Err>;
 
     /// Checks if the map contains a specific key.
-    fn contains(&self, key: &K) -> Result<bool, Self::Err>;
+    async fn contains(&self, key: &K) -> Result<bool, Self::Err>;
 
     /// Lets you run a function on the specified key
-    fn inspect<F>(&self, key: &K, f: F) -> Result<(), Self::Err>
+    async fn inspect<F>(&self, key: &K, f: F) -> Result<(), Self::Err>
     where
-        F: FnMut(Option<&V>);
+        F: FnMut(Option<&V>) + Send;
 
     /// runs a function that can mutate the value if it exists
-    fn mutate<F>(&self, key: &K, f: F) -> Result<(), Self::Err>
+    // returns true, if the value was mutated
+    async fn mutate<F>(&self, key: &K, f: F) -> Result<bool, Self::Err>
     where
-        F: FnMut(Option<&mut V>);
+        F: FnMut(&mut V) + Send;
 
     /// mutates the value if it exists with the fiven FnMut, or inserts the given value
-    fn mutate_or_insert<F>(&self, key: K, mut f: F, value: V) -> Result<(), Self::Err>
+    async fn mutate_or_insert<F>(&self, key: K, f: F, value: V) -> Result<(), Self::Err>
     where
-        F: FnMut(&mut V),
+        F: FnMut(&mut V) + Send,
+        K: 'async_trait,
+        V: 'async_trait,
     {
-        let v = self.contains(&key)?;
+        let v = self.contains(&key).await?;
         if v {
-            let newf = |m: Option<&mut V>| match m {
-                Some(m) => f(m),
-                _ => {}
-            };
-            self.mutate(&key, newf)?;
+            self.mutate(&key, f).await?;
         } else {
-            self.insert(key, value)?;
+            self.insert(key, value).await?;
         }
         Ok(())
     }
 
     /// runs the method on each of the values in the storage
-    fn for_each<F>(&self, f: F) -> Result<(), Self::Err>
+    async fn for_each<F>(&self, f: F) -> Result<(), Self::Err>
     where
-        F: FnMut((&K, &V));
+        F: FnMut((&K, &V)) + Send;
 
     /// runs the method on each of the values in the storage
-    fn for_each_mut<F>(&self, f: F) -> Result<(), Self::Err>
+    async fn for_each_mut<F>(&self, f: F) -> Result<(), Self::Err>
     where
-        F: FnMut((&K, &mut V));
-}
-
-/// trait to give the most basic of key value storage functionality,
-/// when having ownership
-pub trait MutKeyValueStore<K, V> {
-    type Err;
-    /// Inserts a key-value pair into the map.
-
-    /// If the map did not have this key present, None is returned.
-
-    /// If the map did have this key present, the value is updated, and the old value is returned.
-    /// The key is not updated, though; this matters for types that can be == without
-    /// being identical.
-    fn insert(&mut self, key: K, value: V) -> Result<Option<V>, Self::Err>;
-
-    fn remove(&mut self, key: &K) -> Result<Option<V>, Self::Err>;
-
-    // fn get(&self, key: &Key) -> Result<Option<&Value>, Self::Err>;
-    fn mutate<F>(&mut self, key: &K, f: F) -> Result<(), Self::Err>
-    where
-        F: FnMut(Option<&mut V>);
-
-    /// This is what replaces the .get method, in the way that it allows
-    fn inspect<F>(&self, key: &K, f: F) -> Result<(), Self::Err>
-    where
-        F: FnMut(Option<&V>);
-
-    fn contains(&self, key: &K) -> Result<bool, Self::Err>;
-
-    /// mutates the value if it exists with the fiven FnMut, or inserts the given value
-    fn mutate_or_insert<F>(&mut self, key: K, mut f: F, value: V) -> Result<(), Self::Err>
-    where
-        F: FnMut(&mut V),
-    {
-        let v = self.contains(&key)?;
-        if v {
-            let newf = |m: Option<&mut V>| match m {
-                Some(m) => f(m),
-                _ => {}
-            };
-            self.mutate(&key, newf)?;
-        } else {
-            self.insert(key, value)?;
-        }
-        Ok(())
-    }
-
-    fn for_each<F>(&self, f: F) -> Result<(), Self::Err>
-    where
-        F: FnMut((&K, &V));
-
-    fn for_each_mut<F: FnMut((&K, &mut V))>(&mut self, f: F) -> Result<(), Self::Err>;
+        F: FnMut((&K, &mut V)) + Send;
 }
 
 /// Trait that enables to get a cloned value for the given key
+#[async_trait]
 pub trait GetOwned<K, V> {
     type Err;
     /// Gets a owned clone of the data in the map
-    fn get_owned(&self, key: &K) -> Result<Option<V>, Self::Err>;
+    async fn get_owned(&self, key: &K) -> Result<Option<V>, Self::Err>;
 }
 
+#[async_trait]
 impl<T, K, V> GetOwned<K, V> for T
 where
-    T: KeyValueStore<K, V, Err = Box<dyn std::error::Error>>,
-    V: Clone,
+    T: KeyValueStore<K, V, Err = Box<dyn std::error::Error>> + Send + Sync,
+    K: Send + Sync,
+    V: Clone + Send,
 {
     type Err = Box<dyn std::error::Error>;
-    fn get_owned(&self, key: &K) -> Result<Option<V>, Self::Err> {
+    async fn get_owned(&self, key: &K) -> Result<Option<V>, Self::Err> {
         let mut outer = None;
         self.inspect(key, |v| match v {
             Some(v) => outer = Some(v.clone()),
             None => outer = None,
-        })?;
+        })
+        .await?;
         Ok(outer)
     }
 }
 /// Trait that enables to get a cloned value for the given key
+#[async_trait]
 pub trait GetBTreeMap<K, V> {
     type Err;
     /// Gets a owned clone of the data in the map
-    fn btreemap(&self) -> Result<BTreeMap<K, V>, Self::Err>;
+    async fn btreemap(&self) -> Result<BTreeMap<K, V>, Self::Err>;
 }
 
+#[async_trait]
 impl<T, K, V> GetBTreeMap<K, V> for T
 where
-    T: KeyValueStore<K, V, Err = Box<dyn std::error::Error>>,
-    V: Clone,
-    K: Clone + Ord,
+    T: KeyValueStore<K, V, Err = Box<dyn std::error::Error>> + Send + Sync,
+    V: Clone + Send,
+    K: Clone + Ord + Send + Sync,
 {
     type Err = Box<dyn std::error::Error>;
-    fn btreemap(&self) -> Result<BTreeMap<K, V>, Self::Err> {
+    async fn btreemap(&self) -> Result<BTreeMap<K, V>, Self::Err> {
         let mut map = BTreeMap::new();
         self.for_each(|(k, v)| {
             map.insert(k.clone(), v.clone());
-        })?;
+        })
+        .await?;
         Ok(map)
     }
 }
 /// Trait that enables to get a cloned value for the given key
+#[async_trait]
 pub trait GetHashMap<K, V> {
     type Err;
     /// Gets a owned clone of the data in the map
-    fn hashmap(&self) -> Result<HashMap<K, V>, Self::Err>;
+    async fn hashmap(&self) -> Result<HashMap<K, V>, Self::Err>;
 }
 
+#[async_trait]
 impl<T, K, V> GetHashMap<K, V> for T
 where
-    T: KeyValueStore<K, V, Err = Box<dyn std::error::Error>>,
-    V: Clone,
-    K: Clone + std::hash::Hash + Eq,
+    T: KeyValueStore<K, V, Err = Box<dyn std::error::Error>> + Send + Sync,
+    V: Clone + Send,
+    K: Clone + std::hash::Hash + Eq + Send + Sync,
 {
     type Err = Box<dyn std::error::Error>;
-    fn hashmap(&self) -> Result<HashMap<K, V>, Self::Err> {
+    async fn hashmap(&self) -> Result<HashMap<K, V>, Self::Err> {
         let mut map: HashMap<K, V> = Default::default();
         self.for_each(|(k, v)| {
             map.insert(k.clone(), v.clone());
-        })?;
+        })
+        .await?;
         Ok(map)
     }
 }
